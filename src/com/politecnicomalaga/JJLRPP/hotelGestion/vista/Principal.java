70b2f2537adac39ba101349700824d5faad54d7b
ChemadeUnamuno <147400155+ChemadeUnamuno@users.noreply.github.com>
2023-12-07 13:17:28 +0100

    Entendemos usar Objetos

--- src/com/politecnicomalaga/JJLRPP/hotelGestion/vista/Principal.java
+++ src/com/politecnicomalaga/JJLRPP/hotelGestion/vista/Principal.java
@@ -1,5 +1,9 @@
 package com.politecnicomalaga.JJLRPP.hotelGestion.vista;
 
+import com.politecnicomalaga.JJLRPP.hotelGestion.modelo.Cliente;
+
+import com.politecnicomalaga.JJLRPP.hotelGestion.modelo.Hotel;
+
 public class Principal {
     
     public static void main(String[] args) {
@@ -15,6 +19,18 @@ public class Principal {
         
         //ESCANER PEDIR DATOS
         //private Controlador trabajador;
+    	
+    	Cliente misClientes[] = new Cliente[1];
+    	Hotel miHotel[] = new Hotel[1];
+    	
+    	miHotel[0] = new Hotel ("Costa del Sol", "Su Casita", "Pito", "696969",
+    			"+69 696969", "hotel@gmail.com", misClientes);	
+    	
+    	misClientes[0] = new Cliente ("Juan", "Martínez", "Pérez", "Su Casa", "33", "+33 3333",
+    			"juanito@gmail.com", 33, "33/33/3333");
+    	
+    	System.out.println(miHotel[0]);
+    	System.out.println(misClientes[0]);
         
     }
     
