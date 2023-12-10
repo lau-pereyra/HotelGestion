f6df8afa9c74f5714504bfc74c27e87d44aed65f
ChemadeUnamuno <147400155+ChemadeUnamuno@users.noreply.github.com>
2023-12-10 20:50:49 +0100

    Arreglado Promocion

--- src/com/politecnicomalaga/JJLRPP/hotelGestion/modelo/Promocion.java
+++ src/com/politecnicomalaga/JJLRPP/hotelGestion/modelo/Promocion.java
@@ -1,97 +1,7 @@
 package com.politecnicomalaga.JJLRPP.hotelGestion.modelo;
 
-import java.util.Scanner;
-
 public class Promocion {
 
-<<<<<<< HEAD
-    private int edadMax;
-    private int edadMin;
-    private String nombrePromo;
-    
-    
-    
-    public Promocion(int edadMax, int edadMin, String nombrePromo) {
-        this.edadMax = edadMax;
-        this.edadMin = edadMin;
-        this.setNombrePromo(nombrePromo);
-    }
-    
-public static int edadMaxAñadePromo() {
-        
-        Scanner sc = new Scanner(System.in);
-        String respuesta;
-        int iRespuesta;
-    
-        System.out.println("Introduce la Edad Maxima para la promocion");
-    
-        respuesta = sc.nextLine();
-        iRespuesta = Integer.valueOf(respuesta);
-    
-        return iRespuesta;
-        
-    }
-    
-    public static int edadMinAñadePromo() {
-        
-        Scanner sc = new Scanner(System.in);
-        String respuesta;
-        int iRespuesta;
-    
-        System.out.println("Introduce la Edad Minima para la promocion");
-    
-        respuesta = sc.nextLine();
-        iRespuesta = Integer.valueOf(respuesta);
-    
-        return iRespuesta;
-        
-    }
-    
-    public static String nombreAñadePromo() {
-        
-        Scanner sc = new Scanner(System.in);
-        String respuesta = "";
-        
-        System.out.println("Introduce un Nombre para la promocion.");
-        
-        respuesta = sc.nextLine();
-        
-        return respuesta;
-        
-    }
-
-    public String getNombrePromo() {
-        return nombrePromo;
-    }
-
-    public void setNombrePromo(String nombrePromo) {
-        this.nombrePromo = nombrePromo;
-    }
-    
-    public int getEdadMax() {
-        return edadMax;
-    }
-
-    public void setEdadMax(int edadMax) {
-        this.edadMax = edadMax;
-    }
-
-    public int getEdadMin() {
-        return edadMin;
-    }
-
-    public void setEdadMin(int edadMin) {
-        this.edadMin = edadMin;
-    }
-    
-    public String toString() {
-        return "- Nobmbre: " + nombrePromo + "\n" + "- Edad Minima: " + edadMin + "\n" + "- Edad Maxima: "
-                + edadMax ;
-    }
-    
-    
-}
-=======
 	private int edadMax;
 	private int edadMin;
 	private String nombrePromo;
@@ -132,4 +42,3 @@ public static int edadMaxAñadePromo() {
     }
 	
 }
->>>>>>> b2e530eaec8ec1ce98a70906a5df19a1cf20035d
